7bb8cec9ccdbd2d05a5993dd1de0430436584220
sam2702 <Singh.santosh2702@gmail.com>
2018-07-01 22:37:15 +0530

    commit

---
+++ Hierarchical_Clustering/hc1.py
@@ -0,0 +1,33 @@
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset with pandas
+dataset=pd.read_csv('Mall_Customers.csv')
+X=dataset.iloc[:, [3,4]].values
+
+#using the dendogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram=sch.dendrogram(sch.linkage(X,method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distance')
+plt.show()
+
+#fittng hierarchical clustering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc=AgglomerativeClustering(n_clusters=5,affinity='euclidean',linkage='ward')
+y_hc=hc.fit_predict(X)
+
+# visualising the clusters
+plt.scatter(X[y_hc==0,0],X[y_hc==0,1],s=100,c='red',label='Careful')
+plt.scatter(X[y_hc==1,0],X[y_hc==1,1],s=100,c='blue',label='Standard')
+plt.scatter(X[y_hc==2,0],X[y_hc==2,1],s=100,c='green',label='Target')
+plt.scatter(X[y_hc==3,0],X[y_hc==3,1],s=100,c='cyan',label='Careless')
+plt.scatter(X[y_hc==4,0],X[y_hc==4,1],s=100,c='magenta',label='Sensible')
+plt.title('Clusters of clients')
+plt.xlabel('Annual Income(ks)')
+plt.ylabel('spending Score(1-100)')
+plt.legend()
+plt.show()
